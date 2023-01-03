924931100557de2326adef96bc556b8f9affa055
Edelyne Keisha <114095264+edeliciouso@users.noreply.github.com>
2023-01-03 17:28:44 +0700

    Delete FSM-Project directory

--- FSM-Project/mylib/mylib.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void states(int s0, int s1, int i1, int i2, int* S0, int* S1){
-    *S0 = s0 & (!i2) | s1 & i1;
-    *S1 = s0 & i1 | s1 & (!i2) | ((!s0) & (!s1)) & ((!i1) & (!i2));
-}
