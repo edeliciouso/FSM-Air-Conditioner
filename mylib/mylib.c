b275a966dd00e48d9533b1b3a9fb478aa252fd81
Edelyne Keisha <114095264+edeliciouso@users.noreply.github.com>
2023-01-20 22:31:56 +0700

    Update mylib.c

--- mylib/mylib.c
+++ mylib/mylib.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-// void states(int s0, int s1, int i1, int i2, int* S0, int* S1){
-//     *S0 = s0 & (!i2) | s1 & i1;
-//     *S1 = s0 & i1 | s1 & (!i2) | ((!s0) & (!s1)) & ((!i1) & (!i2));
-// }
+void states(int s0, int s1, int b1, int b2, int* S0, int* S1){
+    // *S0 = ((!b1) & (!b2) & s0) | (b1 & s0 & s1);
+    // *S1 = ((b2 & (!s0) & (!s1))) | (b1 & s0 & (!s1)) | ((!b1) & (!b2) & s1);
+    // *S0 = (b1 & (!b2) & (!s0) & s1) | ((!b1) & (!b2) & s0 & (!s1)) | (b1 & b2 & s0 & (!s1));
+    // *S1 = ((!b1) & b2 & (!s0) & (!s1)) | ((!b1) & (!b2) & (!s0) & (s1)) | (b1 & (!b2) & s0 & (!s1));
+    *S0 = (b1 & (!b2) & s1) | ((!b1) & (!b2) & s0) | (b1 & b2 & s0);
+    *S1 = ((!b1) & (!b2) & s1) | (b1 & b2 & s1) | (b1 & (!b2) & s0) | ((!b1) & b2 & (!s0) & (!s1));
+}
 
-void states(int s0, int s1, int b1, int b2, int* S0, int* S1, int* o0, int* o1){
-    *S0 = (!b1) & (!b2) & s0 | b1 & s0 & s1;
-    *S1 = b2 & (!s0) & (!s1) | b1 & s0 & (!s1) | (!b1) & (!b2) & s1;
-    *o0 = s0 & (!s1);
-    *o1 = (!s0) & s1;
+void outputs(int s0, int s1, int* o0, int* o1){
+    *o0 = (s0 & (!s1));
+    *o1 = ((!s0) & s1);
 }
