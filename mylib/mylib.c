13e5975b11fa8ff5af7fad36d7cee1bcbd356ae8
Edelyne Keisha <114095264+edeliciouso@users.noreply.github.com>
2023-01-20 14:12:19 +0700

    Update mylib.c

--- mylib/mylib.c
+++ mylib/mylib.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 // void states(int s0, int s1, int i1, int i2, int* S0, int* S1){
@@ -11,18 +10,4 @@ void states(int s0, int s1, int b1, int b2, int* S0, int* S1, int* o0, int* o1){
     *S1 = b2 & (!s0) & (!s1) | b1 & s0 & (!s1) | (!b1) & (!b2) & s1;
     *o0 = s0 & (!s1);
     *o1 = (!s0) & s1;
-=======
-#include <stdio.h>
-
-// void states(int s0, int s1, int i1, int i2, int* S0, int* S1){
-//     *S0 = s0 & (!i2) | s1 & i1;
-//     *S1 = s0 & i1 | s1 & (!i2) | ((!s0) & (!s1)) & ((!i1) & (!i2));
-// }
-
-void states(int s0, int s1, int b1, int b2, int* S0, int* S1, int* o0, int* o1){
-    *S0 = (!b1) & (!b2) & s0 | b1 & s0 & s1;
-    *S1 = b2 & (!s0) & (!s1) | b1 & s0 & (!s1) | (!b1) & (!b2) & s1;
-    *o0 = s0 & (!s1);
-    *o1 = (!s0) & s1;
->>>>>>> 7881863747079c689b3f675ae5958986e4e8f591
 }
