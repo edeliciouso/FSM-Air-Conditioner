ea1a0e3ae4d7603fde2634f11dd65133473b509c
Edelyne Keisha <114095264+edeliciouso@users.noreply.github.com>
2023-01-20 22:32:22 +0700

    Update mylib.h

--- mylib/mylib.h
+++ mylib/mylib.h
@@ -1,1 +1,2 @@
-void states(int s0, int s1, int b1, int b2, int* S0, int* S1, int* o0, int* o1);
+void states(int s0, int s1, int b1, int b2, int* S0, int* S1);
+void outputs(int s0, int s1, int* o0, int* o1);
